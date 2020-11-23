edd488007c68a97c6797f697b31d586acd04f6b5
Douglas <douglas.cosmo7@gmail.com>
2020-11-23 08:33:47 -0300

    criação do projeto e teste integração de módulos

---
+++ src/test/kotlin/com/douglas/compra/CompraApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.douglas.compra
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CompraApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
---
+++ starter/src/main/kotlin/com.douglas.compra/CompraApplication.kt
@@ -0,0 +1,11 @@
+package com.douglas.compra
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CompraApplication
+
+fun main(args: Array<String>) {
+    runApplication<CompraApplication>(*args)
+}
